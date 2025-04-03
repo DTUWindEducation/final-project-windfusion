a242d19f46ab8014640be1c8c64cb623d81af530
Tautvydas <s243092@student.dtu.dk>
2025-04-03 11:15:12 +0200

    added a file to run the functions of week8

---
+++ examples/code_week8.py
@@ -0,0 +1,14 @@
+import finalproject
+import matplotlib.pyplot as plt
+import pandas as pd
+import os
+
+file_name = "Location1.py"
+df, basic_stats = finalproject.load_observations_data(file_name)
+
+
+variable_name = 'windspeed_100m'
+site_index = 1
+starting_time = '2017-05-01 00:00:00'
+ending_time = '2017-05-01 23:59:59'
+fig, ax = finalproject.plot_timeseries(variable_name, site_index, starting_time, ending_time)
